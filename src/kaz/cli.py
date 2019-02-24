76729f37a09add79205f2e776ca790c27217b759
Kazuya Takei <attakei@gmail.com>
2019-02-25 01:23:04 +0900

    Define first 'init' command

--- src/kaz/cli.py
+++ src/kaz/cli.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 import click
 
 from . import __version__ as ver
+from .core import Application
 
 
 @click.group()
@@ -14,6 +17,21 @@ def version():
     """Display version"""
     msg = 'kaz {}'
     click.echo(msg.format(ver))
+
+
+@cmd.command()
+@click.argument('root', type=click.Path(resolve_path=True))
+def init(root):
+    """Initialize kaz env"""
+    root = Path(root)
+    if root.exists():
+        click.echo(
+            'Path is already exists. Please specify non-exists path.',
+            err=True)
+        return
+    app = Application(root)
+    app.create()
+    # TODO: Display follow-up message
 
 
 def main():
