9e5c9b9bb2894712cce7592a505f33d45576d306
Kazuya Takei <attakei@gmail.com>
2019-02-25 15:35:58 +0900

    Pre-define finder for application root

--- src/kaz/cli.py
+++ src/kaz/cli.py
@@ -1,3 +1,4 @@
+import os
 import os.path
 from pathlib import Path
 import textwrap
@@ -6,6 +7,17 @@ import click
 
 from . import __version__ as ver
 from .core import Application, DEFAULT_ROOT_DIR
+
+
+ROOT_DIR_ENV = 'KAZ_ROOT'
+
+
+def find_appdir():
+    """Find application directory for CLI
+    """
+    if ROOT_DIR_ENV in os.environ:
+        return Path(os.environ[ROOT_DIR_ENV])
+    return Path.home() / DEFAULT_ROOT_DIR
 
 
 @click.group()
@@ -45,7 +57,9 @@ def init(root):
     Initialize is finished in {root}.
 
     Next your steps:
-        1. Set bin directory of kaz into enviroment variables PATH
+        1. Set your root directory into environment
+        export KAZ_ROOT={root}
+        2. Set bin directory of kaz into enviroment variables PATH
         export PATH={root}/bin:$PATH
     """.format(root=root)).strip())
 
