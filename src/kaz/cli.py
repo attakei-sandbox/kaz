16f4cf9944e69bb7c408ac607106c63ad5bb1175
Kazuya Takei <attakei@gmail.com>
2019-02-25 15:47:45 +0900

    Short 'add' command
    - create repo dir
    - create catalog/metatada

--- src/kaz/cli.py
+++ src/kaz/cli.py
@@ -64,6 +64,32 @@ def init(root):
     """.format(root=root)).strip())
 
 
+@cmd.command()
+@click.argument('name')
+@click.argument('asset')
+def add(name, asset):
+    root = find_appdir()
+    if not root.exists():
+        click.echo(
+            click.style('WARNING:', fg='yellow', bg='magenta'), nl=False)
+        click.echo(
+            click.style(' Root directory is not exists', fg='magenta'))
+        return
+    app = Application(root)
+    if app.repo_exists(name):
+        click.echo(click.style(
+            'Application "{}" is already registered.'.format(name),
+            fg='magenta'))
+        return
+    github_path = click.prompt('Enter GitHub project path(xxx/yyy)')
+    if len(github_path.split('/')) != 2:
+        click.echo(click.style('Invalid path is passed', fg='magenta'))
+        return
+    app.add_repo(
+        name,
+        {'vendor': 'github', 'path': github_path, 'asset': asset})
+
+
 def main():
     """Etrypoint"""
     cmd()
--- src/kaz/core.py
+++ src/kaz/core.py
@@ -1,9 +1,12 @@
 """Core of kaz
 """
+import json
 import logging
 import logging.config
 import sys
 from pathlib import Path
+
+from .catalog import Catalog
 from .logging import get_logconf
 
 
@@ -47,3 +50,15 @@ class Application(object):
         # Logging handler need to exists output folder
         logging.config.dictConfig(get_logconf(self.log_dir))
         self.logger.info('Initialized!')
+
+    def repo_exists(self, name):
+        return (self.repo_dir / name).exists()
+
+    def add_repo(self, name, meta):
+        repo_dir = self.repo_dir / name
+        repo_dir.mkdir(self.MOD_DIR)
+        repo_metadata = repo_dir / 'metadata.json'
+        repo_metadata.write_text(
+            json.dumps(meta, indent=True))
+        repo_catalog = Catalog()
+        repo_catalog.save(repo_dir / 'catalog.json')
